860da742582af595fc5316f9b44e24793ef83d72
Octupus <rey.dev.ne@gmail.com>
2025-03-05 02:38:51 +0100

    updated

--- ebpf_helloworld.py
+++ ebpf_helloworld.py
@@ -19,12 +19,17 @@ return 0;
 }
 """
 b=BPF(text=program) # here , all u need to do is pass the string "program"as a parameter when creating BPF object
-syscall = b.get_syscall_fnname("execve") #here we are attaching ebpf program to an event that is the system call execve, use to excute a prgrm! , basically whenever anyone or anything strats a program that excutes on this machine , execve() will be called which will trigger the ebpf program 
-#but why b.get_syscall_fnname? becuz execve is the standard interfac ein linux but we want to use bcc to get the name of the function that implements it in the kernel , it actually depends on the chip architecture of the machine
-b.attach_kprobe(event=syscall, fn_name="hello")# now , deal with syscall as the name of the function in the kernel. now just use a kprobe to attach hello function(our ebpf prgrm) to that event
-#congrats, at this point ur ebpf program is loaded into the kernel and attached into an event, whenever a new executable gets launched on the machine ur prgrm will be triggered
-b.trace_print()#this is the remaining python code to read the output of the tracing by the kernel and write it on the screen
+syscall = b.get_syscall_fnname("execve") #here we are attaching ebpf program to an event #that is the system call execve, use to excute a prgrm! , basically whenever anyone or #anything strats a program that excutes on this machine , execve() will be called which #will trigger the ebpf program 
+#but why b.get_syscall_fnname? becuz execve is the standard interfac ein linux but we want #to use bcc to get the name of the function that implements it in the kernel , it actually #depends on the chip architecture of the machine
+b.attach_kprobe(event=syscall, fn_name="hello")
+# now , deal with syscall as the name of the function in the kernel. 
+#now just use a kprobe to attach hello function(our ebpf prgrm) to that event
+#congrats, at this point ur ebpf program is loaded into the kernel and attached into an event, 
+#whenever a new executable gets launched on the machine ur prgrm will be triggered
+b.trace_print()#this is the remaining python code to read the output of the tracing by 
+#the kernel and write it on the screen
 # trace_print() function will loop indefinitely until u stop the program , use ctrl+c
+
 
 
 
